66e5ff96abadc798129200d7adaa11d46b61e21d
Alexander Nasokin <alexnas2python@gmail.com>
2020-09-28 10:51:16 +0300

    Prepare structure for events.

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,16 @@
 import { DomListener } from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+  }
+
   // Return component template
   toHTML() {
     return '';
+  }
+
+  init() {
+    this.initDOMListeners();
   }
 }

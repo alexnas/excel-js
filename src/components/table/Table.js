a3ce3e6165228d6d2eca63a6360eccf29abc9aa8
Alexander Nasokin <alexnas2python@gmail.com>
2020-11-12 17:12:36 +0300

    Finalize table resize.

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,8 +1,22 @@
 import { ExcelComponent } from '../../core/ExcelComponent';
 import { createTable } from './table.template';
+import { resizeHandler } from './table.resize';
+import { shouldResize } from './table.functions';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
+
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    });
+  }
+
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
+    }
+  }
 
   toHTML() {
     return createTable(20);
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -4,24 +4,33 @@ const CODES = {
 };
 
 function createRow(index, content) {
+  const resize = index
+    ? `<div class='row-resize' data-resize='row'></div>`
+    : '';
   return `
-    <div class='row'>
-      <div class='row-info'>${index ? index : ''}</div>
+    <div class='row' data-type='resizable' data-elem='row'>
+      <div class='row-info'>
+        ${index ? index : ''}
+        ${resize}
+      </div>
       <div class='row-data'>${content}
       </div>
     </div>
   `;
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column">${col}</div>
+    <div class='column' data-type='resizable' data-col=${index}>
+      ${col}
+      <div class='col-resize' data-resize='col'></div>
+    </div>
   `;
 }
 
-function toCell() {
+function toCell(_, col) {
   return `
-    <div class="cell" contenteditable=""></div>
+    <div class="cell" contenteditable="" data-col=${col}></div>
   `;
 }
 
